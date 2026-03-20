893f8d559c942108f3ee0e4eefdb5eeb737d57e9
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-03-20 14:18:14 +0530

    Grades calculated

--- DurwaPahariya_FrontEnd/Js/DurwaPahariya_student_analyzer.js
+++ DurwaPahariya_FrontEnd/Js/DurwaPahariya_student_analyzer.js
@@ -22,20 +22,11 @@ const students = [
 
     attendance: 91
   },
-  {
-    name:"Aman",
-    marks: [
-      { subject: "Math", score: 35 },
-      { subject: "English", score: 60 },
-      { subject: "Science", score: 55 },
-      { subject: "History", score: 50 },
-      { subject: "Computer", score: 65 }
-    ],
-    attendance: 70
-  }
+ 
 ];
 
-// Calculation of total marks of Students 
+//1) Calculation: TOTAL MARKS
+
 function calculateTotalMarks(student) {
   let total = 0;
   for (let sub of student.marks) {
@@ -43,7 +34,37 @@ function calculateTotalMarks(student) {
   }
   return total;
 }
-console.log("Total marks scored by Lalit:", calculateTotalMarks(students[0]));
-console.log("Total marks scored by Rahul:", calculateTotalMarks(students[1]));
+//console.log("Total marks scored by Lalit:", calculateTotalMarks(students[0]));
+//console.log("Total marks scored by Rahul:", calculateTotalMarks(students[1]));
 
-//Calculating Average Marks
+//2)Calculation: AVERAGE MARKS
+
+function calculateAverageMarks(student){
+    const total = calculateTotalMarks(student);
+    
+    return total/student.marks.length;
+}
+//console.log("Avg marks scored by Lalit:", calculateAverageMarks(students[0]));
+//console.log("Avg marks scored by Rahul:", calculateAverageMarks(students[1]));
+
+
+
+//3)Calculating : Grades Logic
+function Grade(student) {
+
+  let avg =calculateAverageMarks(student);
+  if (avg >= 85) {
+    return "A";
+  } else if (avg >= 70) {
+    return "B";
+  } else if (avg >= 50) {
+    return "C";
+  } else {
+    return "Fail";
+  }
+}
+//console.log("Lalit Grade:", Grade(students[0]));
+//console.log("Rahul Grade:", Grade(students[1]));
+
+
+//4)calculating:SUBJECT-WISE HIGHEST Marks
