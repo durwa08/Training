0ee696f0beda127bbffc3cc1314cce5fe461a264
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-08 13:56:43 +0530

    Program that checks even and odd numbers

--- Durwa_Pahariya_java_training/session1/basic_java/EvenOdd.java
+++ Durwa_Pahariya_java_training/session1/basic_java/EvenOdd.java
@@ -1,3 +1,24 @@
+import java.util.*;
 public class EvenOdd {
+    public boolean isEven(int num){
+        return num % 2 == 0;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        EvenOdd check = new EvenOdd();
+        
+        System.out.println("Enter the number :");
+        int num = sc.nextInt();
+
+        if(check.isEven((num))){
+            System.out.println("Number is Even");
+        }else{
+            System.out.println("Number is odd");
+        }
+        sc.close();
+
+    }
+
     
 }
