c92226b7856c0ab4f4da3a03a2ddbb3aeac586ff
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-08 15:15:10 +0530

    Made pattern printer completely dynamic

--- Durwa_Pahariya_java_training/session1/basic_java/PatternPrint.java
+++ Durwa_Pahariya_java_training/session1/basic_java/PatternPrint.java
@@ -1,16 +1,24 @@
+import java.util.Scanner;
+
 public class PatternPrint {
     
-    public void printRtTriangle(int rows){
-        for(int i =1;i <= rows; i++){
-            for(int j = 1;j <=i ;j++){
+    public void printRtTriangle(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-
     }
+
     public static void main(String[] args) {
-        PatternPrint print = new PatternPrint();
-        print.printRtTriangle(3);
+        Scanner sc = new Scanner(System.in);
+        PatternPrint obj = new PatternPrint();
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+        obj.printRtTriangle(n);
+
+        sc.close();
     }
 }
