da1e2f31ba5880ff1924d8150e105281c9b420c0
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-12 02:16:13 +0530

    User Model Added

---
+++ Durwa_Pahariya_java_training/Java/session2/nucleusteq/src/test/java/com/training/nucleusteq/NucleusteqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.nucleusteq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleusteqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
