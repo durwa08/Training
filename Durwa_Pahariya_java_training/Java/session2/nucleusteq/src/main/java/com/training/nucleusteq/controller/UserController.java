f5a241421164ea8810e2cb40139792ea77a87971
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-12 23:35:17 +0530

    Code cleanup

--- Durwa_Pahariya_java_training/Java/session2/nucleusteq/src/main/java/com/training/nucleusteq/controller/UserController.java
+++ Durwa_Pahariya_java_training/Java/session2/nucleusteq/src/main/java/com/training/nucleusteq/controller/UserController.java
@@ -17,33 +17,18 @@ public class UserController {
         this.userService = userService;
     }
 
-    // GET /user helps fetch all users
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    // GET /users/{id} → fetch user by id
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id) {
+    public User getUser(@PathVariable int id) {
         return userService.getUserById(id);
     }
 
-    // POST /users → create user
     @PostMapping
     public String createUser(@RequestBody User user) {
         return userService.createUser(user);
-    }
-
-    // DELETE /users/{id} → delete user
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable int id) {
-        return userService.deleteUser(id);
-    }
-
-    // PUT /users/{id} → update user
-    @PutMapping("/{id}")
-    public String updateUser(@PathVariable int id, @RequestBody User user) {
-        return userService.updateUser(id, user);
     }
 }
