5593bad72fdc80b95c24c98a1ed45dcf7da0f374
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-12 22:39:21 +0530

    NotificationController added

---
+++ Durwa_Pahariya_java_training/Java/session2/nucleusteq/src/main/java/com/training/nucleusteq/controller/NotificationController.java
@@ -0,0 +1,23 @@
+package com.training.nucleusteq.controller;
+
+import com.training.nucleusteq.service.NotificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notify")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    // Constructor Injection ✅
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @GetMapping
+    public String sendNotification() {
+        return notificationService.triggerNotification();
+    }
+}
