242df8c5a89054a7e9d96baf18d60bb3e618e67d
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-15 12:27:24 +0530

    Initial project setup

---
+++ Durwa_Pahariya_java_training/Java/session3/AdvanceTraining/src/main/java/com/spring/AdvanceTraining/AdvanceTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.spring.AdvanceTraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceTrainingApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
