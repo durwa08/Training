fbff7db051783013d8cc1e43e60a83c51e5a429b
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-15 12:33:13 +0530

    Model Layer Added

---
+++ Durwa_Pahariya_java_training/Java/session3/AdvanceTraining/src/main/java/com/spring/AdvanceTraining/model/User.java
@@ -0,0 +1,30 @@
+package com.spring.AdvanceTraining.model;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String role;
+
+    public User(int id, String name, int age, String role) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getRole() {
+        return role;
+    }
+
+
+}
