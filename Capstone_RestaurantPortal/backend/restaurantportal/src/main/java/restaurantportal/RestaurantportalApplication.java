4ed5a52988e6dd0f66a86308e3c1dffeef1379c0
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-20 15:36:41 +0530

    Initial backend setup

---
+++ Capstone_RestaurantPortal/backend/restaurantportal/src/main/java/restaurantportal/RestaurantportalApplication.java
@@ -0,0 +1,14 @@
+package restaurantportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantportalApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestaurantportalApplication.class, args);
+	}
+
+}
---
+++ Capstone_RestaurantPortal/backend/restaurantportal/src/test/java/restaurantportal/RestaurantportalApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurantportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
