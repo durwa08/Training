f5831da5af867cf0d12c662b06eca8e364bf432d
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-04-21 16:23:29 +0530

    Restro entity added

--- Capstone_RestaurantPortal/backend/restaurantportal/src/main/java/restaurantportal/entity/Restaurant.java
+++ Capstone_RestaurantPortal/backend/restaurantportal/src/main/java/restaurantportal/entity/Restaurant.java
@@ -1,5 +1,31 @@
 package restaurantportal.entity;
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+import lombok.*;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private RestaurantStatus status;
 
+    @ManyToOne
+    @JoinColumn(name = "Owner_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "restaurant")
+    private List<Category> categories;
+
+    @OneToMany(mappedBy = "restaurant")
+        private List<MenuItem> menuItems;
 }
