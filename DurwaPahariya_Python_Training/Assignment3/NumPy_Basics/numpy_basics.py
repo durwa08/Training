92cec114899331932115582f43c51e40c306c520
Durwaa Pahariya <durwapahariya08@gmail.com>
2026-06-22 08:31:30 +0530

    Implemented addition and multiplication operations and also 3*3 matrix

--- DurwaPahariya_Python_Training/Assignment3/NumPy_Basics/numpy_basics.py
+++ DurwaPahariya_Python_Training/Assignment3/NumPy_Basics/numpy_basics.py
@@ -28,4 +28,34 @@ print("Maximum:", np.max(numbers))
 print("Minimum:", np.min(numbers))
 print("Sum:", np.sum(numbers))
 
+# -----------------------------------
+# Question 3: Perform Addition and
+# Multiplication on Two Arrays
+# -----------------------------------
+
+first_array = np.array([2, 4, 6])
+second_array = np.array([1, 3, 5])
+
+print("\nQuestion 3: Array Operations")
+
+print("Addition:")
+print(first_array + second_array)
+
+print("\nMultiplication:")
+print(first_array * second_array)
+
+
+# -----------------------------------
+# Question 4: Create a 3x3 Matrix
+# -----------------------------------
+
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+print("\nQuestion 4: 3x3 Matrix")
+print(matrix)
+
 
